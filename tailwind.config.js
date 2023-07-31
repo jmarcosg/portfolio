51ff0bd11ad62649c1790b143581d3b9ed6783ec
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-07-31 11:37:39 -0300

    💄 add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        paleLavander: '#DBD7FB',
+        mistyRose: '#FFE2E3',
+      },
     },
   },
   plugins: [],
