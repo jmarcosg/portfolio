1d82db4a5cc7fb5569b3d1dbce1334417f29ceeb
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-08-01 23:19:50 -0300

    ✨ export components in a barrel file

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header";
+export { default as Project } from "./Project";
+export { default as SectionDivider } from "./SectionDivider";
+export { default as SectionHeading } from "./SectionHeading";
+export { default as SectionSubheading } from "./SectionSubheading";
