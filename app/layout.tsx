131ca42cdec72f93552a6a3a3bea1586d4a4108a
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-08-01 23:18:18 -0300

    ✨ add active section context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,33 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import { Header } from "@/components";
+import ActiveSectionContextProvider from "@/context/ActiveSection";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import React from "react";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Juan Marcos Gonzalez | Portfolio',
-  description:
-    'Juan Marcos is a Fullstack Software Developer oriented to the Frontend Development.',
+  title: "Juan Marcos Gonzalez | Portfolio",
+  description: "Juan Marcos is a Fullstack Software Developer oriented to the Frontend Development."
 };
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="!scroll-smooth">
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
+        <div className="bg-paleLavander absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
+        <div className="bg-mistyRose absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
+
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
+      </body>
     </html>
   );
 }
