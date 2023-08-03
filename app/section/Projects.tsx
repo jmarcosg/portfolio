a8d09a0c18aa07b31b9e72449f98b1dd93df34c9
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-08-03 14:32:15 -0300

    ✨ add section in view reference

--- app/section/Projects.tsx
+++ app/section/Projects.tsx
@@ -1,10 +1,12 @@
 import { Project, SectionHeading } from "@/components";
+import { useSectionInView } from "@/hooks/useSectionInView";
 import { projectsData } from "@/lib/data";
 import React from "react";
 
 export default function Projects() {
+  const { ref } = useSectionInView("Projects");
   return (
-    <section id="projects" className="scroll-mt-28 mb-28">
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>Featured Projects</SectionHeading>
 
       <div>
