03b9df9b3d4af255dfdad4d6be67bea85a14e1c8
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-08-02 23:22:05 -0300

    ✨ componentize skill stack

--- app/section/Skills.tsx
+++ app/section/Skills.tsx
@@ -1,29 +1,14 @@
 "use client";
 
-import { SectionHeading, SectionSubheading } from "@/components";
+import { SectionHeading, SkillStack } from "@/components";
 import { useSectionInView } from "@/hooks/useSectionInView";
 import {
-  backendSkillsData,
-  frontendAndDesignSkillsData,
+  codingLanguagesAndToolsData,
+  databasesData,
   languagesData,
-  mobileSkillsData,
-  toolsSkillsData
+  librariesAndFrameworksData,
+  otherToolsSkillsData
 } from "@/lib/data";
-import { motion } from "framer-motion";
-
-const fadeInAnimationVariants = {
-  initial: {
-    opacity: 0,
-    y: 100
-  },
-  animate: (index: number) => ({
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: 0.05 * index
-    }
-  })
-};
 
 export default function Skills() {
   const { ref } = useSectionInView("Skills");
@@ -32,150 +17,11 @@ export default function Skills() {
     <section id="skills" ref={ref} className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
       <SectionHeading>My skills</SectionHeading>
 
-      <motion.article
-        variants={fadeInAnimationVariants}
-        initial="initial"
-        whileInView="animate"
-        viewport={{
-          once: true
-        }}
-        custom={"languages"}
-      >
-        <SectionSubheading>Languages</SectionSubheading>
-        <motion.ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-          {languagesData.map((skill, index) => (
-            <motion.li
-              className="bg-white borderBlack rounded-xl px-5 py-3"
-              key={index}
-              variants={fadeInAnimationVariants}
-              initial="initial"
-              whileInView="animate"
-              viewport={{
-                once: true
-              }}
-              custom={index}
-            >
-              {skill.name} {skill.flag}
-            </motion.li>
-          ))}
-        </motion.ul>
-      </motion.article>
-
-      <motion.article
-        variants={fadeInAnimationVariants}
-        initial="initial"
-        whileInView="animate"
-        viewport={{
-          once: true
-        }}
-        custom={"frontendAndDesign"}
-      >
-        <SectionSubheading>Front-end</SectionSubheading>
-        <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-          {frontendAndDesignSkillsData.map((skill, index) => (
-            <motion.li
-              className="bg-white borderBlack rounded-xl px-5 py-3"
-              key={index}
-              variants={fadeInAnimationVariants}
-              initial="initial"
-              whileInView="animate"
-              viewport={{
-                once: true
-              }}
-              custom={index}
-            >
-              {skill}
-            </motion.li>
-          ))}
-        </ul>
-      </motion.article>
-
-      <motion.article
-        variants={fadeInAnimationVariants}
-        initial="initial"
-        whileInView="animate"
-        viewport={{
-          once: true
-        }}
-        custom={"backend"}
-      >
-        <SectionSubheading>Back-end</SectionSubheading>
-        <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-          {backendSkillsData.map((skill, index) => (
-            <motion.li
-              className="bg-white borderBlack rounded-xl px-5 py-3"
-              key={index}
-              variants={fadeInAnimationVariants}
-              initial="initial"
-              whileInView="animate"
-              viewport={{
-                once: true
-              }}
-              custom={index}
-            >
-              {skill}
-            </motion.li>
-          ))}
-        </ul>
-      </motion.article>
-
-      <motion.article
-        variants={fadeInAnimationVariants}
-        initial="initial"
-        whileInView="animate"
-        viewport={{
-          once: true
-        }}
-        custom={"mobile"}
-      >
-        <SectionSubheading>Mobile</SectionSubheading>
-        <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-          {mobileSkillsData.map((skill, index) => (
-            <motion.li
-              className="bg-white borderBlack rounded-xl px-5 py-3"
-              key={index}
-              variants={fadeInAnimationVariants}
-              initial="initial"
-              whileInView="animate"
-              viewport={{
-                once: true
-              }}
-              custom={index}
-            >
-              {skill}
-            </motion.li>
-          ))}
-        </ul>
-      </motion.article>
-
-      <motion.article
-        variants={fadeInAnimationVariants}
-        initial="initial"
-        whileInView="animate"
-        viewport={{
-          once: true
-        }}
-        custom={"mobile"}
-      >
-        <SectionSubheading>Tools</SectionSubheading>
-        <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-          {toolsSkillsData.map((skill, index) => (
-            <motion.li
-              className="bg-white borderBlack rounded-xl px-5 py-3"
-              key={index}
-              variants={fadeInAnimationVariants}
-              initial="initial"
-              whileInView="animate"
-              viewport={{
-                once: true
-              }}
-              custom={index}
-            >
-              {skill}
-            </motion.li>
-          ))}
-        </ul>
-      </motion.article>
+      <SkillStack title={"LANGUAGES"} skillsData={languagesData} />
+      <SkillStack title={"CODING LANGUAGES AND TOOLS"} skillsData={codingLanguagesAndToolsData} />
+      <SkillStack title={"LIBRARIES AND FRAMEWORKS"} skillsData={librariesAndFrameworksData} />
+      <SkillStack title={"DATABASES"} skillsData={databasesData} />
+      <SkillStack title={"OTHER"} skillsData={otherToolsSkillsData} />
     </section>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,4 @@ export { default as Project } from "./Project";
 export { default as SectionDivider } from "./SectionDivider";
 export { default as SectionHeading } from "./SectionHeading";
 export { default as SectionSubheading } from "./SectionSubheading";
+export { default as SkillStack } from "./SkillStack";
