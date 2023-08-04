58516f94861bdedb2fc9594e92387f0f36cd6af1
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-08-04 09:56:28 -0300

    🧑‍💻 improve section componentization

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,6 @@
 export { default as Header } from "./Header";
 export { default as Project } from "./Project";
-export { default as SectionDivider } from "./SectionDivider";
-export { default as SectionHeading } from "./SectionHeading";
-export { default as SectionSubheading } from "./SectionSubheading";
+export { default as SectionDivider } from "./Section/Divider";
+export { default as SectionHeading } from "./Section/Heading";
+export { default as SectionSubheading } from "./Section/Subheading";
 export { default as SkillStack } from "./SkillStack";
