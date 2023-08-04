2c4e7ad4ccbc51c76d56bf0a4ac367ccf59676d2
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-08-04 10:33:19 -0300

    🔥 remove unnecesary route

--- app/section/index.ts
+++ app/section/index.ts
@@ -1,7 +1,7 @@
-export { default as About } from "./About/index";
-export { default as Contact } from "./Contact/index";
-export { default as Education } from "./Education/index";
-export { default as Experience } from "./Experience/index";
-export { default as Intro } from "./Intro/index";
-export { default as Projects } from "./Projects/index";
-export { default as Skills } from "./Skills/index";
+export { default as About } from "./About";
+export { default as Contact } from "./Contact";
+export { default as Education } from "./Education";
+export { default as Experience } from "./Experience";
+export { default as Intro } from "./Intro";
+export { default as Projects } from "./Projects";
+export { default as Skills } from "./Skills";
