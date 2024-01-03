3329b7e35256866116507d4db045102d83b78be9
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-01-03 18:25:50 -0300

    💬 update description

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="mb-10 px-4 text-center text-gray-400">
       <small className="mb-2 block text-xs">&copy; {currentYear} jmarcosg. All rights reserved.</small>
       <p className="text-xs">
-        <span className="font-semibold">Built with ❤️</span>
+        <span className="font-semibold">Built with ❤️ and ☕️</span>
       </p>
     </footer>
   );
