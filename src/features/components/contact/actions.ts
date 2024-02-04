833e761b1af3a5d07cf7ec1fbcec2b8cacbffc40
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-03 21:26:08 -0300

    ✨ add edge functions

--- src/features/components/contact/actions.ts
+++ src/features/components/contact/actions.ts
@@ -6,6 +6,9 @@ import React from "react";
 import { Resend } from "resend";
 import ContactForm from "./ContactForm";
 
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: TContactForm) => {
