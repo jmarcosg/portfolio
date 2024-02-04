7ba8d7ccb646e7300aa79e14ccf14a0a8a366d3a
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-04 03:12:59 -0300

    💄 add opacity when there's also a window hash

--- src/components/header/background.tsx
+++ src/components/header/background.tsx
@@ -1,19 +1,20 @@
-"use client"
+"use client";
 
-import { useScroll } from "@/hooks"
-import { cn } from "@/lib/utils"
-import { useSearchParams } from "next/navigation"
+import { useScroll } from "@/hooks";
+import { cn } from "@/lib/utils";
+import { useSearchParams } from "next/navigation";
 
 export const Background = () => {
-  useSearchParams()
-  const { scrolled } = useScroll(0)
+	useSearchParams();
+	const { scrolled } = useScroll(0);
+	const pageHash = window.location.hash;
 
-  return (
-    <div
-      className={cn(
-        "bg-background/70 absolute left-0 top-0 z-[-1] h-full w-full opacity-0 shadow-md backdrop-blur-md transition-all",
-        { "opacity-100": scrolled }
-      )}
-    />
-  )
-}
+	return (
+		<div
+			className={cn(
+				"bg-background/70 absolute left-0 top-0 z-[-1] h-full w-full opacity-0 shadow-md backdrop-blur-md transition-all",
+				{ "opacity-100": scrolled || pageHash },
+			)}
+		/>
+	);
+};
