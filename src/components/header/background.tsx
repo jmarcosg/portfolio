8e742a730618382af84f99eceeee9fa69a0df1cf
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-04 23:20:45 -0300

    🔥 window hash isn't working

--- src/components/header/background.tsx
+++ src/components/header/background.tsx
@@ -7,13 +7,12 @@ import { useSearchParams } from "next/navigation";
 export const Background = () => {
 	useSearchParams();
 	const { scrolled } = useScroll(0);
-	const pageHash = window.location.hash;
 
 	return (
 		<div
 			className={cn(
 				"bg-background/70 absolute left-0 top-0 z-[-1] h-full w-full opacity-0 shadow-md backdrop-blur-md transition-all",
-				{ "opacity-100": scrolled || pageHash },
+				{ "opacity-100": scrolled },
 			)}
 		/>
 	);
